09bd0b9938a75d0d826b4fa6e66a99458e0f1101
Lucas Martins <lucasmartinsk11@gmail.com>
2026-02-07 04:57:02 -0300

    Add Perssua cask

---
+++ Casks/perssua.rb
@@ -0,0 +1,10 @@
+cask "perssua" do
+  version "0.13.5"
+  sha256 "d063d9dacb206a86eaa3542da13ff57207f4596d82c8bc213ee4a7531b89c96d"
+
+  url "https://downloads.perssua.com/Perssua-#{version}-arm64.dmg"
+  name "Perssua"
+  homepage "https://perssua.com/en/downloads"
+
+  app "Perssua.app"
+end
